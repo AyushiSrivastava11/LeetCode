bcdb205c7af4d8f3bf86651e5df299e237980d4d
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2023-10-08 18:08:30 +0530

    Time: 6 ms (100.00%) | Memory: 54.9 MB (97.78%) - LeetSync

--- 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.java
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.java
@@ -1,31 +1,54 @@
 class Solution {
     public int minimumCardPickup(int[] cards)
     {
-      Map <Integer,Integer> checkingHash= new HashMap<Integer,Integer>();
-      int start=0,end=0,ans=Integer.MAX_VALUE;
-      boolean checked=false;
-      while(end < cards.length)
-      {
-          Integer point=checkingHash.get(cards[end]);
-          if(point == null)
-          {
-              checkingHash.put(cards[end],end);
-          }
-          else
-          {
-              while(cards[start] != cards[end])
-              {
-               checkingHash.remove(cards[start]);
-               start++;  
-              }
-               ans=Math.min(ans,(end-start)+1);
-               System.out.print(ans);
-                  checked=true;
-                  checkingHash.put(cards[start],end);
-                  start++;
-          }
-          end++;
-      }
-      return (!checked)?-1:ans;
+    // //   Map <Integer,Integer> checkingHash= new HashMap<Integer,Integer>();
+    // //   int start=0,end=0,ans=Integer.MAX_VALUE;
+    // //   boolean checked=false;
+    // //   while(end < cards.length)
+    // //   {
+    // //       Integer point=checkingHash.get(cards[end]);
+    // //       if(point == null)
+    // //       {
+    // //           checkingHash.put(cards[end],end);
+    // //       }
+    // //       else
+    // //       {
+    // //           while(cards[start] != cards[end])
+    // //           {
+    // //            checkingHash.remove(cards[start]);
+    // //            start++;  
+    // //           }
+    // //            ans=Math.min(ans,(end-start)+1);
+    // //            System.out.print(ans);
+    // //               checked=true;
+    // //               checkingHash.put(cards[start],end);
+    // //               start++;
+    // //       }
+    // //       end++;
+    // //   }
+    //   return (!checked)?-1:ans;
+    boolean[] checked= new boolean[1000001]; //all are intially false
+    boolean present=false;
+    int start=0,ans=Integer.MAX_VALUE,n=cards.length;
+    for(int end=0;end<n;end++)
+    {
+        if(checked[cards[end]])
+        {
+            while(cards[start] != cards[end])
+            {
+                checked[cards[start]]=false;
+                start++;
+            }
+            ans=Math.min(ans,(end-start+1));
+            present=true;
+            start++;
+        }
+        else
+        {
+            checked[cards[end]]=true;
+        }
+    }
+    
+    return (present)?ans:-1;
     }
 }
