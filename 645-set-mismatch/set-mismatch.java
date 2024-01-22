834e5440e73a5d3c54f168017cdd6d36f98f5dc5
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2024-01-22 20:51:52 +0530

    Time: 3 ms (81.72%) | Memory: 45.9 MB (8.03%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] findErrorNums(int[] arr)
+     {
+         int i=0;
+        while(i<arr.length)
+        {
+            int correct=arr[i]-1;
+            if(arr[i] != arr[correct])
+            {
+                swapIndex(arr, i, correct);
+            }
+            else
+            {
+                i++;
+            }
+        }
+        
+        for(int index=0;index<arr.length;index++)
+        {
+            if(arr[index]!=index+1)
+            {
+                return new int[]{arr[index],index+1};
+            }  
+        }
+        return new int[]{-1,-1};
+    }
+
+     void swapIndex(int[] arr,int first,int second)
+    {
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
