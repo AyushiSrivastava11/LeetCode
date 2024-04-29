22b67703698c1cc67807137f780ea76cc3295bef
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2024-04-29 20:21:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (100.00%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
@@ -1,16 +1,27 @@
 class Solution {
-    public int addedInteger(int[] nums1, int[] nums2) 
-    {
-     int sum1=0,sum2=0;
-     for(int nums : nums1)
-     {
-        sum1+=nums;
-     }
-     for(int nums:nums2)
-     {
-        sum2+=nums;
-     }
-     return (sum2-sum1)/nums1.length;
+    // public int addedInteger(int[] nums1, int[] nums2) 
+    // {
+    //  int sum1=0,sum2=0;
+    //  for(int nums : nums1)
+    //  {
+    //     sum1+=nums;
+    //  }
+    //  for(int nums:nums2)
+    //  {
+    //     sum2+=nums;
+    //  }
+    //  return (sum2-sum1)/nums1.length;
 
+    // }
+
+    public int addedInteger(int[] nums1, int[] nums2)
+    {
+        int max1=0,max2=0;
+        for(int index=0;index<nums1.length;index++)
+        {
+            max1=Math.max(nums1[index],max1);
+            max2=Math.max(nums2[index],max2);
+        }
+        return max2-max1;
     }
 }
