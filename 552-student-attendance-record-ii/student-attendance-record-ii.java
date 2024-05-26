ff487e99909830b8da454413364b6575ba8edafa
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2024-05-26 16:06:58 +0530

    Time: 2 ms (100.00%) | Memory: 40.7 MB (93.50%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.java
+++ 552-student-attendance-record-ii/student-attendance-record-ii.java
@@ -1,37 +1,54 @@
 class Solution {
-
-    private static final int MOD = 1000000000 + 7;
+    static final int mod = 1000000007;
 
     public int checkRecord(int n) {
-        int prevDP[][] = new int[2][3];
-        prevDP[0][0] = 1;
-        prevDP[0][1] = 1;
-        prevDP[0][2] = 1;
-        prevDP[1][0] = 1;
-        prevDP[1][1] = 1;
-        prevDP[1][2] = 1;
-        
-        for(int i = 1; i <= n; i++){
-            int newDP[][] = new int[2][3];
-            for(int a = 0; a < 2; a++){
-                for(int l = 0; l < 3; l++){
-                    //Pick P
-                    newDP[a][l] += prevDP[a][2];
-                    if(a > 0){
-                        //Pick A
-                        newDP[a][l] += prevDP[a - 1][2];
-                        newDP[a][l] %= MOD;
-                    }
-                    if(l > 0){
-                        // Pick L
-                        newDP[a][l] += prevDP[a][l - 1];
-                        newDP[a][l] %= MOD;
-                    }
+        long[][] mat = new long[][]{
+            {1, 1, 1, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0},
+            {1, 1, 1, 1, 1, 1},
+            {0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0}
+        };
+
+        long[][] res = pow(mat, n);
+        long ans = 0;
+
+        long[] initial = {1, 0, 0, 0, 0, 0};
+        for(int i=0; i<=5; i++){
+            long sum = 0;
+            for(int j=0; j<=5; j++){
+                sum = (sum + res[i][j] * initial[j]) % mod;
+            }
+            ans = (ans + sum) % mod;
+        }
+        return (int) (ans);
+    }
+
+    public long[][] pow(long[][] mat, int n) {
+        long[][] ret = {{1, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 1}};
+        while (n > 0) {
+            if ((n % 2) == 1) {
+                ret = multiply(ret, mat);
+            }
+            n /= 2;
+            mat = multiply(mat, mat);
+        }
+        return ret;
+    }
+
+    public long[][] multiply(long[][] a, long[][] b) {
+        long[][] c = new long[6][6];
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= 5; j++) {
+                c[i][j] = 0; 
+                for (int k = 0; k <= 5; k++) {
+                    c[i][j] = (c[i][j] + a[i][k] * b[k][j]) % mod;
                 }
             }
-            prevDP = newDP;
         }
-        
-        return prevDP[1][2];
+        return c;    
     }
+
+
 }
