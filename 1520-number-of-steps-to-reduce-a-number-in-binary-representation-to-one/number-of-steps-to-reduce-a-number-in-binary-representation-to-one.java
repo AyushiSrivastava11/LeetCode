88704503ddd0f16bca9c73c301cea338b282bea0
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2024-05-29 19:35:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (57.23%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,33 +1,16 @@
 class Solution {
     public int numSteps(String s) {
-        int l = s.length() - 1;
-        int carry = 0;
-        int count = 0;
-        
-        while (l > 0) {
-            // even number with carry = 0, result even
-            if (Character.getNumericValue(s.charAt(l)) + carry == 0) {
-                carry = 0;
-                count++;
-            // odd number with carry = 1, result even
-            } else if (Character.getNumericValue(s.charAt(l)) + carry == 2) {
+        int res = 0, carry = 0;
+
+        for(int i = s.length() - 1; i> 0; i--){
+            res++;
+            if(s.charAt(i) - '0' + carry == 1){
                 carry = 1;
-                count++;
-            // even number with carry = 1, result odd
-            // odd number with carry = 0, result odd
-            } else {
-                carry = 1;
-                count += 2;
+                res ++;
             }
-            l--;
+
         }
-        
-        // last digit 1 needs to add a carried over digit 1, which gives 10.
-        // So need one more divide to make it 1 (one more step)
-        if (carry == 1) {
-            count++;
-        }
-        
-        return count;
+
+        return res + carry;
     }
 }
