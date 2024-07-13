ae55c441302c3a0afb00f7d83f8f79d3768cf743
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2024-07-13 23:37:49 +0530

    Time: 0 ms (100.00%) | Memory: 57.9 MB (5.67%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,21 +1,28 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        k = k % nums.length;
-        int[] temp = new int[k];
+        //Observe how [1,2,3,4,5,6,7] is rotating and turning into
+        //[5,6,7,1,2,3,4]
+        // which means from backward(n-k to k) numbers are reversed, and from front 0 to n-k-1 are reverse
+        k=k%nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+
+        reverse(nums,k,nums.length-1);
+
+
+
+        //[4,3,2,1,7,6,5] rev the whole now 0-n which is the ans
+
         
-        // Copy the last k elements to the temp array
-        for (int index = 0; index < k; index++) {
-            temp[index] = nums[nums.length - k + index];
-        }
-        
-        // Shift the remaining elements to the right
-        for (int index = nums.length - 1; index >= k; index--) {
-            nums[index] = nums[index - k];
-        }
-        
-        // Copy the elements from temp array to the beginning
-        for (int index = 0; index < k; index++) {
-            nums[index] = temp[index];
+    }
+    void reverse(int[] nums, int start,int end)
+    {
+     while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
