313a813b287010dfaf5d9ed6936f4b5e7d4b1118
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2024-07-25 15:21:53 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (14.74%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,29 +1,30 @@
 class Solution {
-    public void sortColors(int[] nums) 
-    {
-    //  Arrays.sort(nums);  
-     int zeroes=0,ones=0,n=nums.length;
-     //O(N)
-     for(int num : nums)
-     {
-        if(num == 1) ones++;
-        else if(num == 0) zeroes++;
-     }
+    public void sortColors(int[] nums) {
+        int low=0,high=nums.length-1,mid=0;
+        while(mid <= high)
+        {
+            if(nums[mid] == 0)
+            {
+                //Swap arr[low],arr[mid]
+                int temp=nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
 
-     for(int index=0;index<n;index++)
-     {
-        if( index < zeroes)
-        {
-            nums[index]=0;
+                //Increment low as well as mid as from 0 to mid-1 it is already sorted and our low is pointing to the first interval of 1 so that if we encounter 0 we can swap it with the first position of 1
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
         }
-        else if(index>=zeroes && index<(zeroes+ones))
-        {
-            nums[index]=1;
-        }
-        else
-        {
-            nums[index]=2;
-        }
-     }
     }
 }
