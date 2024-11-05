c47df11f6d187106319d1b5f02b78490a518a853
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2024-11-05 15:02:08 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (51.06%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean divisorGame(int n) {
+       /*
+        * Observe : if n = 1 means no alice does not win
+        if n = 2 means alice wins
+        */
+        return (n%2==0);
+    }
+}
