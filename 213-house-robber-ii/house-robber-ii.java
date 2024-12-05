0fb983c5ac680f29261e7e12178321a56d040c8c
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2024-12-05 21:12:50 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (43.56%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,44 +1,21 @@
 class Solution {
-    public int rob(int[] nums) 
-    {
+    public int rob(int[] nums) {
         if(nums.length == 1) return nums[0];
-
-        int[] arr1=new int[nums.length];
-        int[] arr2=new int[nums.length];
-
-        for(int index=0;index<nums.length;index++)
-        {
-            if(index!=0){
-                arr1[index]=nums[index];
-            }
-            if(index!=nums.length-1)
-            {
-                arr2[index]=nums[index];
-            }
+        int prev1=0,prev2=nums[0],prev3=0,prev4=nums[1];
+        for(int index = 1;index<nums.length-1;index++){
+            int pick = nums[index] + prev1;
+            int no_pick = 0 + prev2;
+            int curr = Math.max(pick,no_pick);
+            prev1=prev2;
+            prev2=curr;
         }
-
-        return Math.max(rob2(arr1),rob2(arr2));
-
-    }
-
-     public int rob2(int[] nums) 
-    {
-        int prev1=nums[0];
-        int prev2=0;
-
-        for(int index=1;index<nums.length;index++)
-        {
-            int take=nums[index];
-            if(index>1)
-            {
-                take+=prev2;
-            }
-
-            int notTake=prev1;
-            int currIndex=Math.max(take,notTake);
-            prev2=prev1;
-            prev1=currIndex;
+        for(int index = 2;index<nums.length;index++){
+            int pick = nums[index] + prev3;
+            int no_pick = 0 + prev4;
+            int curr = Math.max(pick,no_pick);
+            prev3=prev4;
+            prev4=curr;
         }
-        return prev1;
+        return Math.max(prev2,prev4);
     }
 }
