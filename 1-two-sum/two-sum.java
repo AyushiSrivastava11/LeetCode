f9d45c3ad2f802db215f658eeec41fa19e51bf0a
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2025-01-23 11:50:42 +0530

    Time: 3 ms (65.22%) | Memory: 45.5 MB (9.34%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,37 +1,16 @@
-class Solution 
-{
-    public int[] twoSum(int[] nums, int target)
-    {
-        int[] ans={-1,-1};
-       /* 
-        int sum=0;
-        for(int i=0;i<nums.length;i++)
-        {
-           for(int j=i+1;j<nums.length;j++)
-           {
-               if((nums[i]+nums[j]) == target)
-               {
-                   ans[0]=i;
-                   ans[1]=j;
-               }
-           }
-        }*/
-        
-        HashMap<Integer , Integer>x= new HashMap<>(); //Hash bana
-         for(int i=0;i<nums.length;i++) //nums array traverse hua
-         {
-             if(x.containsKey(target-nums[i])) //ye check hua ki map me target-nums[i] present h ki nhi
-             {
-                 ans[0]=i; //agar hua to i ans[0] me gaya
-                 ans[1]=x.get(target-nums[i]); //subtract krke element ka index chala jayega
-                 break;
-             }
-             else
-             {
-                 x.put(nums[i],i); //agar nahi hua to value ko index k saath put krdenge
-             }
-         }
-        
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = { -1, -1 };
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (map.containsKey(target - nums[index])) {
+                ans[0] = map.get(target - nums[index]);
+                ans[1] = index;
+                break;
+            } else {
+                map.put(nums[index], index);
+            }
+        }
         return ans;
     }
 }
