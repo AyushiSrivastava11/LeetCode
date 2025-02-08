624fbef50e5caed6aec19dd106e1b95ac921da49
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2025-02-08 22:09:31 +0530

    Time: 54 ms (98.63%) | Memory: 96.7 MB (95.21%) - LeetSync

---
+++ 2434-design-a-number-container-system/design-a-number-container-system.java
@@ -0,0 +1,53 @@
+class NumberContainers {
+
+    public NumberContainers() {
+        numberToIndices = new HashMap<>();
+        indexToNumbers = new HashMap<>();
+    }
+
+    public void change(int index, int number) {
+        // Update index to number mapping
+        indexToNumbers.put(index, number);
+
+        // Add index to the min heap for this number
+        numberToIndices
+            .computeIfAbsent(number, k -> new PriorityQueue<>())
+            .add(index);
+    }
+
+    public int find(int number) {
+        // If number doesn't exist in our map
+        if (!numberToIndices.containsKey(number)) {
+            return -1;
+        }
+
+        // Get min heap for this number
+        PriorityQueue<Integer> minHeap = numberToIndices.get(number);
+
+        // Keep checking top element until we find valid index
+        while (!minHeap.isEmpty()) {
+            int index = minHeap.peek();
+
+            // If index still maps to our target number, return it
+            if (indexToNumbers.get(index) == number) {
+                return index;
+            }
+
+            // Otherwise remove this stale index
+            minHeap.poll();
+        }
+
+        return -1;
+    }
+
+    // Map to store number -> min heap of indices
+    private Map<Integer, PriorityQueue<Integer>> numberToIndices;
+    // Map to store index -> number
+    private Map<Integer, Integer> indexToNumbers;
+}
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers obj = new NumberContainers();
+ * obj.change(index,number);
+ * int param_2 = obj.find(number);
+ */
