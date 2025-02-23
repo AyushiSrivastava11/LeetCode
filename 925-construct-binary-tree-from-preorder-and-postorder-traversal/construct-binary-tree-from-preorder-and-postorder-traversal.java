d432f19d86f8c8975a33a583f82f9d64e477b1d7
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2025-02-23 21:15:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (41.67%) - LeetSync

---
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private int preIndex = 0;
+    private int postIndex = 0;
+
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        return constructTree(preorder, postorder);
+    }
+
+    // Helper function to recursively build the tree
+    private TreeNode constructTree(int[] preorder, int[] postorder) {
+        // Create a new node with the value at the current preorder index
+        TreeNode root = new TreeNode(preorder[preIndex++]);
+
+        // Recursively construct the left subtree if the root is not the last of
+        // its subtree
+        if (root.val != postorder[postIndex]) {
+            root.left = constructTree(preorder, postorder);
+        }
+
+        // Recursively construct the right subtree if the root is still not the
+        // last of its subtree
+        if (root.val != postorder[postIndex]) {
+            root.right = constructTree(preorder, postorder);
+        }
+
+        // Mark this node and its subtree as fully processed
+        postIndex++;
+        return root;
+    }
+}
