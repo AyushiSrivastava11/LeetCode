4e0816babb2c1f5c39544929002e747aadd49a59
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2025-05-04 00:05:28 +0530

    Time: 2 ms (100.00%) | Memory: 50.4 MB (53.07%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
@@ -1,21 +1,32 @@
+
 class Solution {
-    public int minDominoRotations(int[] tops, int[] bottoms) {
-        int n = tops.length, res = Integer.MAX_VALUE;
-        int[] face = new int[7];
-        for (int i = 0; i < n; i++) {
-            face[tops[i]]++;
-            if (bottoms[i] != tops[i]) face[bottoms[i]]++;
-        }
-        for (int x = 1; x <= 6; x++) {
-            if (face[x] < n) continue;
-            int maintainTop = 0, maintainBottom = 0;
-            boolean possible = true;
-            for (int i = 0; i < n; i++) {
-                if (tops[i] != x) maintainTop++;
-                if (bottoms[i] != x) maintainBottom++;
+    private int helper(int[] tops, int[] bottoms, int val) {
+        int top_res = 0, bottom_res = 0;
+        for (int i = 0; i < tops.length; i++) {
+            if (tops[i] != val && bottoms[i] != val) {
+                return -1;
+            } else if (tops[i] != val) {
+                top_res++;
+            } else if (bottoms[i] != val) {
+                bottom_res++;
             }
-            if (possible) res = Math.min(res, Math.min(maintainTop, maintainBottom));
         }
-        return res == Integer.MAX_VALUE ? -1 : res;
+        return Math.min(top_res, bottom_res);
+    }
+
+
+
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        
+        
+        int ans = -1;
+        for (int i = 1; i < 7; i++) {
+            int cur_ans = helper(tops, bottoms, i);
+
+            if (cur_ans != -1 && (ans == -1 || ans > cur_ans)) {
+                ans = cur_ans;
+            }
+        }
+        return ans;
     }
 }
