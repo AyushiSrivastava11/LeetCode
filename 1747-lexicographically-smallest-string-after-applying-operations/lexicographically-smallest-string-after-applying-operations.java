373c65aca88b181786e57fd453a2a3798832b38a
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2025-10-19 23:32:50 +0530

    Time: 2 ms (98.88%) | Memory: 42 MB (97.75%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
@@ -2,30 +2,45 @@ class Solution {
 
     public String findLexSmallestString(String s, int a, int b) {
         int n = s.length();
-        boolean[] vis = new boolean[n];
         String res = s;
-        // double the length of s for convenience in extracting the rotated string t
         s = s + s;
-        for (int i = 0; !vis[i]; i = (i + b) % n) {
-            vis[i] = true;
-            for (int j = 0; j < 10; j++) {
-                int kLimit = b % 2 == 0 ? 0 : 9;
-                for (int k = 0; k <= kLimit; k++) {
-                    // before each accumulation, re-truncate t
-                    char[] t = s.substring(i, i + n).toCharArray();
-                    for (int p = 1; p < n; p += 2) {
-                        t[p] = (char) ('0' + ((t[p] - '0' + j * a) % 10));
-                    }
-                    for (int p = 0; p < n; p += 2) {
-                        t[p] = (char) ('0' + ((t[p] - '0' + k * a) % 10));
-                    }
-                    String tStr = new String(t);
-                    if (tStr.compareTo(res) < 0) {
-                        res = tStr;
-                    }
-                }
+        int g = gcd(b, n);
+
+        for (int i = 0; i < n; i += g) {
+            char[] t = s.substring(i, i + n).toCharArray();
+            add(t, n, a, 1);
+            if (b % 2 != 0) {
+                add(t, n, a, 0);
+            }
+            String tStr = new String(t);
+            if (tStr.compareTo(res) < 0) {
+                res = tStr;
             }
         }
         return res;
+    }
+
+    public void add(char[] t, int n, int a, int start) {
+        int minVal = 10;
+        int times = 0;
+        for (int i = 0; i < 10; i++) {
+            int added = ((t[start] - '0') + i * a) % 10;
+            if (added < minVal) {
+                minVal = added;
+                times = i;
+            }
+        }
+        for (int i = start; i < n; i += 2) {
+            t[i] = (char) ('0' + (((t[i] - '0') + times * a) % 10));
+        }
+    }
+
+    public int gcd(int num1, int num2) {
+        while (num2 != 0) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp % num2;
+        }
+        return num1;
     }
 }
