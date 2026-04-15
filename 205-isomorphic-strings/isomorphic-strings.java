52364417f15b980c7fa3a42951a05c08753248c6
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2026-04-15 15:50:59 +0530

    Time: 7 ms (80.29%) | Memory: 43.7 MB (77.49%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,20 +1,12 @@
 class Solution {
-    public boolean isIsomorphic(String s, String t) 
-    {
-        if(s.length() != t.length())
-        {
-            return false;
-        }
-        int[] forS=new int[200];
-        int[] forT=new int[200];
-        for(int index=0;index<t.length();index++)
-        {
-            if(forS[s.charAt(index)] != forT[t.charAt(index)])
-            {
-                return false;
-            }
-            forS[s.charAt(index)] =index+1;
-            forT[t.charAt(index)] =index+1;
+    public boolean isIsomorphic(String s, String t) {
+        int[] m = new int[256];
+        int[] n = new int[256];
+        int length=s.length();
+        for(int index = 0;index<length;index++){
+            if(m[s.charAt(index)]!=n[t.charAt(index)]) return false;
+            m[s.charAt(index)] = index+1;
+            n[t.charAt(index)] = index+1;
         }
         return true;
     }
