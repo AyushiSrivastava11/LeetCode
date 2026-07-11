c96892abe644e905ee289a0eb6fa9723bde23951
Ayushi Srivastava <110414146+AyushiSrivastava11@users.noreply.github.com>
2026-07-11 21:24:52 +0530

    Time: 8 ms (94.15%) | Memory: 47.4 MB (94.64%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -1,64 +1,72 @@
-class Solution {
-    int[] parent;
-    int[] rank;
+public class Solution {
 
     public int countCompleteComponents(int n, int[][] edges) {
-        parent = new int[n];
-        rank = new int[n];
+        // Initialize Union Find and edge counter
+        UnionFind dsu = new UnionFind(n);
+        Map<Integer, Integer> edgeCount = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-
+        // Connect components using edges
         for (int[] edge : edges) {
-            union(edge[0], edge[1]);
+            dsu.union(edge[0], edge[1]);
         }
 
-        Map<Integer, Set<Integer>> componentVertices = new HashMap<>();
-        Map<Integer, Integer> componentEdges = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            int root = find(i);
-            componentVertices.computeIfAbsent(root, k -> new HashSet<>()).add(i);
-        }
-
+        // Count edges in each component
         for (int[] edge : edges) {
-            int root = find(edge[0]);
-            componentEdges.put(root, componentEdges.getOrDefault(root, 0) + 1);
+            int root = dsu.find(edge[0]);
+            edgeCount.put(root, edgeCount.getOrDefault(root, 0) + 1);
         }
 
+        // Check if each component is complete
         int completeCount = 0;
-        for (int root : componentVertices.keySet()) {
-            int numVertices = componentVertices.get(root).size();
-            int expectedEdges = numVertices * (numVertices - 1) / 2;
-
-            if (componentEdges.getOrDefault(root, 0) == expectedEdges) {
-                completeCount++;
+        for (int vertex = 0; vertex < n; vertex++) {
+            if (dsu.find(vertex) == vertex) { // If vertex is root
+                int nodeCount = dsu.size[vertex];
+                int expectedEdges = (nodeCount * (nodeCount - 1)) / 2;
+                if (edgeCount.getOrDefault(vertex, 0) == expectedEdges) {
+                    completeCount++;
+                }
             }
         }
-
         return completeCount;
     }
 
-    int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
+    class UnionFind {
+
+        int[] parent;
+        int[] size; // Tracks size of each component
+
+        UnionFind(int n) {
+            parent = new int[n];
+            size = new int[n];
+            Arrays.fill(parent, -1);
+            Arrays.fill(size, 1);
         }
-        return parent[x];
-    }
 
-    void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX == rootY) return;
+        // Find root of component with path compression
+        int find(int node) {
+            if (parent[node] == -1) {
+                return node;
+            }
+            return parent[node] = find(parent[node]);
+        }
 
-        if (rank[rootX] < rank[rootY]) {
-            parent[rootX] = rootY;
-        } else if (rank[rootX] > rank[rootY]) {
-            parent[rootY] = rootX;
-        } else {
-            parent[rootY] = rootX;
-            rank[rootX]++;
+        // Union by size
+        void union(int node1, int node2) {
+            int root1 = find(node1);
+            int root2 = find(node2);
+
+            if (root1 == root2) {
+                return;
+            }
+
+            // Merge smaller component into larger one
+            if (size[root1] > size[root2]) {
+                parent[root2] = root1;
+                size[root1] += size[root2];
+            } else {
+                parent[root1] = root2;
+                size[root2] += size[root1];
+            }
         }
     }
 }
